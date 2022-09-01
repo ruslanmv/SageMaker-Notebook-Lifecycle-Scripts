84c899b94393868ce0be283a9f02d4e2f7990ad5
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2022-09-01 23:07:58 +0200

    Update on-start.sh

--- scripts/save-conda-environments/on-start.sh
+++ scripts/save-conda-environments/on-start.sh
@@ -1,21 +1,13 @@
-#!/usr/bin/env bash
-
-set -e
-
-PERSISTED_CONDA_DIR="${PERSISTED_CONDA_DIR:-/home/ec2-user/SageMaker/.persisted_conda}"
-
-echo "Setting up persisted conda environments..."
-mkdir -p ${PERSISTED_CONDA_DIR} && chown ec2-user:ec2-user ${PERSISTED_CONDA_DIR}
-
-envdirs_clean=$(grep "envs_dirs:" /home/ec2-user/.condarc || echo "clean")
-if [[ "${envdirs_clean}" != "clean" ]]; then
-    echo 'envs_dirs config already exists in /home/ec2-user/.condarc. No idea what to do. Exiting!'
-    exit 1
-fi
-
-echo "Adding ${PERSISTED_CONDA_DIR} to list of conda env locations"
-cat << EOF >> /home/ec2-user/.condarc
-envs_dirs:
-  - ${PERSISTED_CONDA_DIR}
-  - /home/ec2-user/anaconda3/envs
+sudo -u ec2-user -i <<'EOF'
+unset SUDO_UID
+WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda/
+source "$WORKING_DIR/miniconda/bin/activate"
+for env in $WORKING_DIR/miniconda/envs/*; do
+    BASENAME=$(basename "$env")
+    source activate "$BASENAME"
+    python -m ipykernel install --user --name "$BASENAME" --display-name "Custom ($BASENAME)"
+done
 EOF
+
+echo "Restarting the Jupyter server.."
+restart jupyter-server
